56bcb80a5e41b594cc7572f86212f4c40b783413
Hexa <67880600+HexaRG@users.noreply.github.com>
2022-06-20 21:01:46 -0400

    literally no-one fucking tested this math????????

--- scripts/copyTime.lua
+++ scripts/copyTime.lua
@@ -8,6 +8,9 @@ require 'mp.msg'
 WINDOWS = 2
 UNIX = 3
 
+MINUTE = 60
+HOUR = 60 * 60
+
 local function platform_type()
     local utils = require 'mp.utils'
     local workdir = utils.to_string(mp.get_property_native("working-directory"))
@@ -38,10 +41,6 @@ local function get_clipboard_cmd()
     end
 end
 
-local function divmod(a, b)
-    return a / b, a % b
-end
-
 local function set_clipboard(text) 
     if platform == WINDOWS then
         mp.commandv("run", "powershell", "set-clipboard", text)
@@ -59,11 +58,12 @@ end
 
 local function copyTime()
     local time_pos = mp.get_property_number("time-pos")
-    local minutes, remainder = divmod(time_pos, 60)
-    local hours, minutes = divmod(minutes, 60)
-    local seconds = math.floor(remainder)
-    local milliseconds = math.floor((remainder - seconds) * 1000)
-    local time = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+	print(time_pos)
+	local hours = time_pos / HOUR
+	local minutes = (time_pos / MINUTE) % MINUTE
+	local seconds = time_pos % MINUTE
+	local milliseconds = (time_pos % 1) * 1000
+    local time = string.format("%02d:%02d:%02d.%03d", math.floor(hours), math.floor(minutes), math.floor(seconds), milliseconds)
     if set_clipboard(time) then
         mp.osd_message(string.format("Copied to Clipboard: %s", time))
     else
