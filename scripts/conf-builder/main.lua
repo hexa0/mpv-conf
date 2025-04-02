ce0216d94a5f77a89803e421999e143b4f01efec
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-04-02 05:34:28 -0400

    enable auto build restarting again

--- scripts/conf-builder/main.lua
+++ scripts/conf-builder/main.lua
@@ -186,7 +186,7 @@ local function Build()
 	end
 
 	if restartNeeded then
-		if platform:IsInRange(platform.OS_RANGES.NT) and false then
+		if platform:IsInRange(platform.OS_RANGES.NT) then
 			ShowMessageImmediate("Restarting")
 			local path = mp.get_property("path")
 			local timePosition = mp.get_property("time-pos")
